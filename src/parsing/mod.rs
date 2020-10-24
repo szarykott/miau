d7629e29704b396441a03308210fdb06eef5234c
Radosław Kot <rdkt13@gmail.com>
2020-10-24 16:59:13 +0200

    simplify str_to_key

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -14,27 +14,11 @@ pub(crate) fn str_to_key(input: &str) -> Result<CompoundKey, ConfigurationError>
     for potential_key in input.split_terminator(":") {
         let trimmed_key = potential_key.trim();
 
-        let open_bracket = trimmed_key.starts_with("[");
-        let close_bracket = trimmed_key.ends_with("]");
-
-        if open_bracket && close_bracket {
-            let inner = trimmed_key
-                .strip_prefix("[")
-                .and_then(|s| s.strip_suffix("]"))
-                .map(|s| s.trim());
-
-            match inner {
-                Some(potential_number) => match potential_number.parse::<usize>() {
-                    Ok(i) => result.push(Key::Array(i)),
-                    Err(e) => return Err(ErrorCode::ParsingError(e.to_string()).into()),
-                },
-                None => {
-                    return Err(ErrorCode::ParsingError(format!(
-                        "Expected integer key to start with [ and end with ]. Got {}",
-                        trimmed_key
-                    ))
-                    .into())
-                }
+        if trimmed_key.starts_with("[") && trimmed_key.ends_with("]") {
+            let trimmed_key = trimmed_key[1..trimmed_key.len() - 1].trim();
+            match trimmed_key.parse::<usize>() {
+                Ok(i) => result.push(Key::Array(i)),
+                Err(e) => return Err(ErrorCode::ParsingError(e.to_string()).into()),
             }
         } else {
             result.push(Key::Map(trimmed_key.to_owned()))
